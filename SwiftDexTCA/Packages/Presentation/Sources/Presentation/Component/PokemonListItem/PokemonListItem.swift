b8b51af3fe2acddfe9eeb609c16ab5f77d3190e8
田中裕貴 <tanakayuki@tanakayukinoMac-mini.local>
2025-03-23 00:13:06 +0900

    ポケモン単体コンポーネント作成

--- SwiftDexTCA/Packages/Presentation/Sources/Presentation/Component/PokemonListItem/PokemonListItem.swift
+++ SwiftDexTCA/Packages/Presentation/Sources/Presentation/Component/PokemonListItem/PokemonListItem.swift
@@ -5,20 +5,61 @@
 //  Created by 田中裕貴 on 2025/03/21.
 //
 
+import Domain
 import SwiftUI
-import ComposableArchitecture
 
 struct PokemonListItem: View {
 
-    let store: StoreOf<PokemonListReducer>
+    let pokemon: Pokemon
+    let componentHeight: CGFloat = 200
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            backgroundView()
+                .padding(.top, componentHeight / 4)
+            VStack(spacing: Token.Padding.compact) {
+                pokemonImageView()
+                    .frame(maxWidth: .infinity)
+                pokemonInfoView()
+                    .padding(.bottom, Token.Padding.regular)
+            }
+        }
+        .frame(height: componentHeight)
+    }
+}
+
+private extension PokemonListItem {
+
+    func backgroundView() -> some View {
+        RoundedRectangle(cornerRadius: Token.CornerRadius.pronounced)
+            .fill(.white)
+            .shadow(radius: Token.Shadow.medium)
+    }
+
+    func pokemonImageView() -> some View {
+        AsyncImage(url: pokemon.officialArtworkImageUrl) { image in
+            image.resizable()
+        } placeholder: {
+            Image(.pokemonLogo)
+                .resizable()
+        }
+        .scaledToFit()
+    }
+
+    func pokemonInfoView() -> some View {
+        VStack(spacing: Token.Spacing.tight) {
+            Text("# \(pokemon.id.description)")
+                .font(.title3)
+                .foregroundStyle(.gray)
+            Text(pokemon.name)
+                .font(.title2)
+                .bold()
+        }
     }
 }
 
 #Preview {
-    PokemonListItem(store: Store(initialState: PokemonListReducer.State()) {
-        PokemonListReducer()
-    })
+    PokemonListItem(
+        pokemon: .init(number: 1)
+    )
 }
