b22b29390fcc11046312a55a4d4200f4bc15aca3
田中裕貴 <tanakayuki@tanakayukinoMac-mini.local>
2025-03-28 21:27:39 +0900

    不要なので削除

--- scripts/swift-lint/all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "SwiftLint"
-# 定義されているパスを変数に
-SPM_CLI_COMMON_PATH="Tools/Common/.build/release"
-LINT_RULES_PATH=".swiftlint.yml"
-
-# SwiftLintを実行する
-xcrun --sdk macosx ${SPM_CLI_COMMON_PATH}/mint run swiftlint --config ${LINT_RULES_PATH}
